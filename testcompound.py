581fade41aab57fb7b7927dfad5f5bad6c1f8bbc
Nigel Isidto <1181101303@student.mmu.edu.my>
2022-04-04 03:58:48 +0800

    First Commit

---
+++ testcompound.py
@@ -0,0 +1,45 @@
+"""
+testcompound.py
+Illustrates the use of begin_poly, end_poly, and get_poly to
+create custom turtle shapes.
+"""
+
+from turtle import *
+
+def makeRocketShape():
+    """Creates and registers a new turtle shape for
+    a rocket."""
+    fusilage = ((0,0), (25, 0), (25, 10), (0, 10))
+    noseCone = ((25, 0), (35, 5), (25, 10))
+    fin1 = ((0, 10), (-5, 30), (10, 10))
+    fin2 = ((0, 0), (-5, -20), (10, 0))
+    shape = Shape("compound")
+    shape.addcomponent(noseCone, "pink", "black")
+    shape.addcomponent(fusilage, "red", "black")
+    shape.addcomponent(fin1, "green", "black")
+    shape.addcomponent(fin2, "green", "black")
+    addshape("rocket", shape)
+        
+def main():
+    """Creates two turtles with custom shapes and allows you
+    to drag them around the window."""
+    hideturtle()
+    speed(0)
+    makeRocketShape()
+    sleepy = Turtle(shape = "rocket")
+    sleepy.up()
+    sleepy.goto(100, 50)
+    happy = Turtle(shape = "rocket")
+    # Rotate the shape without changing the heading
+    happy.tilt(90)
+    happy.up()
+    # Well, they don't detect the mouse drag events after all!
+    sleepy.ondrag(lambda x, y: sleepy.goto(x, y))
+    happy.ondrag(lambda x, y: happy.goto(x, y))
+    listen()
+    return "Done!"
+
+if __name__ == '__main__':
+    msg = main()
+    print(msg)
+    mainloop()
---
+++ testpoly.py
@@ -0,0 +1,54 @@
+"""
+testpoly.py
+Illustrates the use of begin_poly, end_poly, and get_poly to
+create custom turtle shapes.
+"""
+
+from turtle import *
+
+def regularPolygon(length, numSides):
+    """Draws a regular polygon.
+    Arguments: the length and number of sides."""
+    interiorAngle = 360 / numSides
+    for count in range(numSides):
+        forward(length)
+        left(interiorAngle)
+
+def makeShape(length, numSides, shapeName):
+    """Creates and registers a new turtle shape with the given name.
+    The shape is a regular polygon with the given length and number
+    of sides.
+    Arguments: the length, number of sides, and shape name."""
+    up()
+    goto(0, 0)
+    setheading(0)
+    begin_poly()
+    regularPolygon(length, numSides)
+    end_poly()
+    shape = get_poly()
+    addshape(shapeName, shape)
+        
+def main():
+    """Creates two turtles with custom shapes and allows you
+    to drag them around the window."""
+    hideturtle()
+    speed(0)
+    makeShape(40, 5, "pentagon")
+    makeShape(20, 8, "octagon")
+    sleepy = Turtle(shape = "pentagon")
+    sleepy.color("brown", "green")
+    sleepy.up() 
+    sleepy.goto(100, 50)
+    sleepy.tilt(90)
+    happy = Turtle(shape = "octagon")
+    happy.color("blue", "pink")
+    happy.up()
+    sleepy.ondrag(lambda x, y: sleepy.goto(x, y))
+    happy.ondrag(lambda x, y: happy.goto(x, y))
+    listen()
+    return "Done!"
+
+if __name__ == '__main__':
+    msg = main()
+    print(msg)
+    mainloop()
